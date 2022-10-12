fead019775347f80369085ea92141f8f63f5ca88
Antonio <netoifpe@gmail.com>
2022-10-12 17:32:13 -0300

    first commit

---
+++ pages/home/index.js
@@ -0,0 +1,177 @@
+let errorMessage = document.querySelector(".label-error-message")
+let input = document.querySelector(".section-search-box-userSearch-input")
+let buttonUserSearch = document.querySelector("#button-userSearch")
+let cLoader = document.querySelector("#c-loader")
+let body = document.querySelector("body")
+let ul = document.querySelector(".list-recent-researches")
+
+async function getAPI(url) {
+    try {
+      const data = await fetch(url);
+      const dataJson = await data.json();
+      return dataJson;
+    } catch (error) {
+       return error;
+    }
+  }
+
+async function editUrl(){
+    activateSpinnerAnimation()
+    const baseURL = `https://api.github.com/users/${input.value}`;
+    const userDetails = await getAPI(baseURL);
+    //  console.log(userDetails)
+    //  console.log(userDetails.message)
+     if (userDetails.message){
+        
+        await setTimeout(()=>{
+            disableSpinnerAnimation()
+            errorMessage.classList = "label-error-message flex"
+            buttonUserSearch.setAttribute("disabled","")
+        },3000)
+        
+
+     } else{
+        
+        await setTimeout(()=>{
+            // disableSpinnerAnimation()
+            errorMessage.classList = "label-error-message"
+            addRecentuserLocalstorage(userDetails.login,userDetails.avatar_url)
+            window.location.href = `../../pages/profile/index.html?user=${userDetails.login}`
+        },3000)
+        
+        
+     }
+    
+}
+
+function activateSpinnerAnimation(){
+    cLoader.classList = "c-loader"
+    cLoader.innerHTML= ""
+}
+
+function disableSpinnerAnimation(){
+    cLoader.classList = ""
+    cLoader.innerHTML= "Ver perfil do github"
+}
+
+function createEvents(){
+    input.addEventListener("keyup", (event)=>{
+ 
+        buttonUserSearch.removeAttribute("disabled")
+        errorMessage.classList = "label-error-message"
+    })
+    
+    buttonUserSearch.addEventListener("click",(event)=>{
+        editUrl()
+    })
+}
+
+function addRecentuserLocalstorage(userLogin, userAvatar){
+    let arrTemp = JSON.parse(localStorage.recentUsers)
+    arrTemp.push({login:userLogin,urlImg:userAvatar})
+     localStorage.recentUsers = JSON.stringify(arrTemp)
+ }
+
+function createListRecentUsers(){
+    
+    let arrTemp = JSON.parse(localStorage.recentUsers)
+    arrTemp.reverse()
+    if (arrTemp.length===0){
+
+    } else if(arrTemp.length<3){
+        for(let index=0; index<arrTemp.length; index++){
+        
+            let li = document.createElement("li")
+            li.classList = "card-recent-user flex flex-col"
+            ul.appendChild(li)
+
+            let img = document.createElement("img")
+            img.src = arrTemp[index].urlImg
+            img.id = `card-recent-user-${index}`
+            img.value = arrTemp[index].login
+            li.appendChild(img)
+
+            
+            let buttonHover = document.createElement("button")
+            buttonHover.innerText = "Acessar este perfil"
+            li.appendChild(buttonHover)
+            buttonHover.value = arrTemp[index].login
+            buttonHover.classList = "button-hover-teste"
+
+                img.addEventListener("click",(event)=>{
+                    window.location.href = `../../pages/profile/index.html?user=${event.target.value}`
+                })
+
+                buttonHover.addEventListener("click",(event)=>{
+                    window.location.href = `../../pages/profile/index.html?user=${event.target.value}`
+                })
+
+                li.addEventListener("mouseover", (event)=>{
+                    buttonHover.classList = "button-hover-teste button-hover-display"
+                })
+
+                li.addEventListener("mouseout", (event)=>{
+                    buttonHover.classList = "button-hover-teste"
+                })
+
+        }
+
+    }else{
+        for(let index=0; index<=2; index++){
+          
+            let li = document.createElement("li")
+            li.classList = "card-recent-user flex flex-col"
+            ul.appendChild(li)
+
+            let img = document.createElement("img")
+            img.src = arrTemp[index].urlImg
+            img.id = `card-recent-user-${index}`
+            img.value = arrTemp[index].login
+            li.appendChild(img)
+
+            
+            let buttonHover = document.createElement("button")
+            buttonHover.innerText = "Acessar este perfil"
+            li.appendChild(buttonHover)
+            buttonHover.value = arrTemp[index].login
+            buttonHover.classList = "button-hover-teste"
+
+                img.addEventListener("click",(event)=>{
+                    window.location.href = `../../pages/profile/index.html?user=${event.target.value}`
+                })
+
+                buttonHover.addEventListener("click",(event)=>{
+                    window.location.href = `../../pages/profile/index.html?user=${event.target.value}`
+                })
+
+                li.addEventListener("mouseover", (event)=>{
+                    buttonHover.classList = "button-hover-teste button-hover-display"
+                })
+
+                li.addEventListener("mouseout", (event)=>{
+                    buttonHover.classList = "button-hover-teste"
+                })
+
+        }
+           
+    }
+   
+}
+
+function startingLocalstorage(){
+    let arrTemp = []
+    if (localStorage.recentUsers){
+        console.log("existe")
+    } else{
+        console.log("opss")
+        localStorage.recentUsers = JSON.stringify(arrTemp)
+    }
+}
+
+window.addEventListener("DOMContentLoaded", (event)=>{
+    startingLocalstorage()
+//  addRecentuserLocalstorage()
+    createListRecentUsers()
+    createEvents()
+  })
+
---
+++ pages/profile/index.js
@@ -0,0 +1,98 @@
+async function getAPI(url) {
+    try {
+      const data = await fetch(url);
+      const dataJson = await data.json();
+      return dataJson;
+    } catch (error) {
+      return error;
+    }
+  }
+  
+  function Header(user) {
+    return `
+          <header class="header flex justify-between items-center">
+             
+            <div class="header-box-left flex">
+            
+                <img class="header-avatar" src="${user.avatar_url}" alt="">
+
+                <div class="header-box-left-infos flex flex-col justify-around ">
+                    <h3 class="header-name">${user.name}</h3>
+                    <h5 class="header-bio">${user.bio}</h5>
+                </div>
+             
+            </div>
+            
+            <div class="header-box-right flex">
+                <button class="button-header-brand-2"><a href="mailto:"${user.email} target="_blank">Email</a></button>
+                <button class="button-header-grey-3" id="button-change-user">Trocar de usuário</button>
+            </div>
+
+          </header>
+      `;
+  }
+
+  function createEventButtonChangeUser(){
+    let button = document.querySelector("#button-change-user")
+    button.addEventListener("click", (event)=>{
+      window.location.href = "../../pages/home/index.html"
+    })
+  }
+
+
+  function Repository(user,repos) {
+    return `
+          <li class="card-repository flex flex-col justify-between">
+              <h3 class="card-repos-name">${repos.name}</h3>
+              <p class="card-repos-description">${repos.description} </p>
+
+              <div class="card-repository-box-buttons flex">
+              <button class="button-card-repos"><a href="${repos.html_url}" target="_blank">Repositório</a></button>
+              <button class="button-card-repos"><a href="https://${user.login}.github.io/${repos.name}/" target="_blank">Demo</a></button>
+              </div>
+          </li>
+      `;
+  }
+
+
+  async function Main(user) {
+    const baseURL = `https://api.github.com/users/${user}`;
+    const userDetails = await getAPI(baseURL);
+    const repositories = await getAPI(`${baseURL}/repos`);
+    
+    //adicionando o nome do user git no title da pagina html
+    let titlePage = document.querySelector("#user-name")
+    titlePage.innerHTML=`${userDetails.login}(${userDetails.name})`
+
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `
+          <main class="container">
+              ${Header(userDetails)}  
+              <div class="loading">Carregando...</div>
+              <ul class="list-repositories flex wrap">
+              ${repositories
+                .map((repository) =>
+                  Repository(userDetails,repository)
+                )
+                .join("")}
+              </ul>
+          </main>
+      `
+    );
+    createEventButtonChangeUser()
+    if (document.querySelector(".list-repositories").children.length > 0) {
+      document.querySelector(".loading").remove();
+    }
+  }
+  
+  
+
+  window.addEventListener("DOMContentLoaded", (event)=>{
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const ParamUser = urlParams.get('user');
+    
+    Main(ParamUser);
+    
+  })
